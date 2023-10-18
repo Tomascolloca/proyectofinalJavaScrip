0977b32f91995db6909b12bdccd188ad51b506f8
Tomas GOMEZ Colloca <tomycollo24@gmail.com>
2023-10-17 23:05:49 -0300

    segundocomit

--- JS/helados.js
+++ JS/helados.js
@@ -1,147 +1,78 @@
-const helados = [
-    {
-      id: 1,
-      titulo: "Torta helada de dulce de leche",
-    
-      precio: 1200,
-    },
-    {
-      id: 2,
-      titulo: "Barrita XL menta granizada",
-   
-      precio: 750,
-    },
-    {
-      id: 3,
-      titulo: "Volcan de Chocolate",
-     
-      precio: 1530,
-    },
-    {
-      id: 4,
-      titulo: "Almendrado Vainilla",
-     
-      precio: 1530,
-    },
-    {
-      id: 5,
-      titulo: "Copa de Frutilla JS",
-   
-      precio: 500,
-    },
-    {
-      id: 6,
-      titulo: "Tiramisu",
-    
-      precio: 2500,
-    },
-    {
-      id: 7,
-      titulo: "Cookies Helada",
-     
-      precio: 970,
-    },
-    {
-      id: 8,
-      titulo: "Limon",
-    
-      precio: 1000,
-    },
+const fs = require('fs');
 
-    {
-        id: 9,
-        titulo: "Sambayon",
-      
-        precio: 1100,
-      },
-  ];
-  
-  //obtencion de elementos
-const agregarBotones = document.querySelectorAll(".boton-item");
-const carritoContenedor = document.querySelector(".carrito-items");
-const carritoTotal = document.querySelector(".carritoprecio-total");
-
-//carrito guardar
-let carrito = [];
-// Cargar productos guardados en el Session Storage
-if (sessionStorage.getItem("carrito")) {
-  carrito = JSON.parse(sessionStorage.getItem("carrito"));
-  actualizarCarritoUI();
-}
-// Función para agregar un producto al carrito
-function agregarItemAlCarrito(id) {
-  const itemExistente = carrito.find((item) => item.id === id);
-  if (itemExistente) {
-    itemExistente.cantidad++;
-  } else {
-    const helado = helados.find((helado) => helado.id === id);
-    carrito.push({ ...helado, cantidad: 1 });
+class ProductoManager {
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.productos = [];
+    this.loadProductos();
   }
-  actualizarCarritoUI();
-  guardarCarritoEnSessionStorage();
-}
 
-// Función para eliminar un producto del carrito
-function eliminarItemDelCarrito(id) {
-  carrito = carrito.filter((item) => item.id !== id);
-  actualizarCarritoUI();
-  guardarCarritoEnSessionStorage();
-}
+  loadProductos() {
+    try {
+      const data = fs.readFileSync(this.filePath, 'utf8');
+      this.productos = JSON.parse(data);
+    } catch (error) {
+      console.log('No se pudo cargar el archivo de productos:', error);
+    }
+  }
 
-//interfaz del carrito
-function actualizarCarritoUI() {
-  carritoContenedor.innerHTML = "";
-  let total = 0;
+  saveProductos() {
+    try {
+      const data = JSON.stringify(this.productos, null, 2);
+      fs.writeFileSync(this.filePath, data, 'utf8');
+    } catch (error) {
+      console.log('No se pudo guardar el archivo de productos:', error);
+    }
+  }
 
-  if (carrito.length === 0) {
-    const mensajeCarritoVacio = document.createElement("div");
-    mensajeCarritoVacio.textContent = "El carrito está vacío";
-    carritoContenedor.appendChild(mensajeCarritoVacio);
-  } else {
-    carrito.forEach((item) => {
-      const carritoItem = document.createElement("div");
-      carritoItem.classList.add("carrito-item");
-      carritoItem.innerHTML = `
-        <span class="carrito-item-titulo">${item.titulo}</span>
-        <div class="selector-cantidad">
-          <!-- Código SVG omitido para ahorrar espacio -->
-        </div>
-        <span class="carrito-item-precio">$${item.precio * item.cantidad}</span>
-      `;
+  addProducto(producto) {
+    this.productos.push(producto);
+    this.saveProductos();
+  }
 
-      carritoContenedor.appendChild(carritoItem);
-      total += item.precio * item.cantidad;
-    });
+  getProducto(id) {
+    return this.productos.find((producto) => producto.id === id);
+  }
 
-    carritoTotal.textContent = `$${total}`;
+  updateProducto(id, updatedProducto) {
+    const index = this.productos.findIndex((producto) => producto.id === id);
+    if (index !== -1) {
+      this.productos[index] = { ...updatedProducto, id };
+      this.saveProductos();
+      return true;
+    }
+    return false;
+  }
+
+  deleteProducto(id) {
+    const index = this.productos.findIndex((producto) => producto.id === id);
+    if (index !== -1) {
+      this.productos.splice(index, 1);
+      this.saveProductos();
+      return true;
+    }
+    return false;
   }
 }
 
-// Session Storage
-function guardarCarritoEnSessionStorage() {
-  sessionStorage.setItem("carrito", JSON.stringify(carrito));
-}
+const productoManager = new ProductoManager('productos.json');
 
-//  eventos botones "Agregar al carrito"
-agregarBotones.forEach((boton, index) => {
-  boton.addEventListener("click", () => {
-    const id = helados[index].id;
-    agregarItemAlCarrito(id);
-  });
-});
+const nuevoProducto = {
+  id: 10,
+  titulo: "Nuevo Helado",
+  precio: 1500,
+};
+productoManager.addProducto(nuevoProducto);
 
-// Pagar
-const botonPagar = document.querySelector(".btn-pagar");
+const productoConsultado = productoManager.getProducto(10);
+console.log('Producto consultado:', productoConsultado);
 
-// evento al "Pagar"
-botonPagar.addEventListener("click", () => {
-  if (carrito.length === 0) {
-    alert("No hay ninguna orden en el carrito");
-  } else {
-    alert("¡Pedido realizado con éxito!");
-    carrito = [];
-    guardarCarritoEnSessionStorage();
-    actualizarCarritoUI();
-    carritoTotal.textContent = "$0";
-  }
-});
+const updatedProduct = {
+  titulo: "Nuevo Helado Actualizado",
+  precio: 1600,
+};
+const updated = productoManager.updateProducto(10, updatedProduct);
+console.log('Producto actualizado:', updated);
+
+const deleted = productoManager.deleteProducto(10);
+console.log('Producto eliminado:', deleted);
